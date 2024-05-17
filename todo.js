421ec9a3dfb5f54824c32eeb73aaf4f5e29233be
borghiste <158812327+borghiste@users.noreply.github.com>
2024-05-17 16:18:28 +0200

    third commit
    adding new todo list

--- todo.js
+++ todo.js
@@ -38,7 +38,7 @@ createtodo(todotext);
 
 
 
-// creo la funzione createtodo: che ha argomento todotext, assehna alla variabile todo il metodo createElement che a sua volta andrà a crere sul html l'elemento li.
+// creo la funzione createtodo: che ha argomento todotext, assegna alla variabile todo il metodo createElement che a sua volta andrà a crere sul html l'elemento li.
 //  sul newtodo uso la proprietà textContent  che inserirà del testo preso dalla variabile todotext
 // successivamente alla variabile snewtodo che contiene il nuovo elemento li aggiungo il metodo append che andrà ad appendere li al suo parennte ul.
 function createtodo(todotext){
@@ -65,54 +65,48 @@ addList.addEventListener('click',(e)=>{let listText = listInputfield.value;
 
   console.log(listText);
 
-  createList(listText);
-
-
-})
-
-function createList(listText){
-  let newheader =document.createElement('header');
-  let newsection = document.createElement('section');
-  
-  
-
-
-  maincontainer.append(newsection);
-   newheader = document.createElement('header');
-  newsection.append(newheader);
-   let newh1 = document.createElement('h1');
-   newh1.textContent= listText;
-   console.log(newh1.textContent);
-  newheader.append(newh1);
-  let newtodoinput = document.createElement('input');
-  newheader.append(newtodoinput);
-  let newtodobutton = document.createElement('button');
-  console.log(newtodobutton);
-  newtodobutton.textContent='add todo';
-  newheader.append(newtodobutton);
-  newtodobutton.
-  
-
-newtodobutton.addEventListener('click',(e)=>{
-    createnewtodoinNewList(newtodotext);
+   createnewList(listText);
   })
-}
 
 
-  function createnewtodoinNewList(todotext){
-    let newtodo = document.createElement('li');
-     newtodo.textContent = todotext;
-  console.log(newtodo)
-  todoContainer.append(newtodo);
-   newtodo.addEventListener('click',function() {newtodo.classList.toggle('done');
+
+// 
+// variabili per creare una nuova lista
+  
+  let newSection = document.createElement('section');
+  
+// funzione per creare nuova lista con nuovo addbutton
+
+function createnewList(listText){
+
+   maincontainer.append(newSection);
+    newheader = document.createElement('header');
+    console.log(listText);
+    newheader.innerText=listText;
+   newSection.append(newheader);
+    let newh1 = document.createElement('h1');
+    newh1.textContent= listText;
+    console.log(newh1.textContent);
+   newheader.append(newh1);
+   let newtodoinput = document.createElement('input');
+   newheader.append(newtodoinput);
+   let newaddButton = document.createElement('button');
+
+   newaddButton.textContent='add todo';
+  
+  newheader.append(newaddButton);
+  newaddButton.addEventListener('click', ()=>{let newtodotext =newtodoinput.value;
+newtodoinput = document.createElement('li');
+   newtodoinput.textContent = newtodotext;
+   newSection.append(newtodoinput);
+})}
+
+// newtodobutton.addEventListener('click',let createnewtodoinNewList= (newtodotext)=>{
+
+
+// }
+//     let createNewAddButton=()=>{let newaddButton = document.createElement('button');
+//   newaddButton.textContent= 'add todo');
+// }
+// }
     
-    })
-  let newclosebutton = document.createElement('button');
-  newclosebutton.textContent='X';
-  newclosebutton.classList.toggle("delete"); 
-  newtodo.append(newclosebutton);
-  newclosebutton.addEventListener('click',(e)=>{let newclosebuttondelete  = newtodo;
-     newclosebuttondelete.remove();
-
-  })
-}
